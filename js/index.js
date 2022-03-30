a4175e20f530e680688f4a118b52170ba66d3dd5
Jeff Workman <jworkhollr@aol.com>
2022-03-30 14:21:12 -0400

    init commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var helloWorld = function () {
+    console.log("Hello Typescript World");
+};
+helloWorld();
+var add = function (x, y) {
+    console.log(x + y);
+};
+add(2, 3);
+add(12, 34);
+var a = 1;
+var b = 44;
+var c = 8;
---
+++ ts/index.ts
@@ -0,0 +1,16 @@
+const helloWorld = () => {
+    console.log("Hello Typescript World");
+}
+
+helloWorld();
+
+const add = (x: number, y: number): void => {
+    console.log(x + y);
+}
+
+add(2, 3);
+add(12, 34);
+
+let a = 1;
+let b = 44;
+let c = 8;
